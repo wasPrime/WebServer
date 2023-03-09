bf9afe035e5c944388589260d716d2840d2272f3
wasPrime <1135677507@qq.com>
2023-03-09 16:15:55 +0000

    🧪 test: add chat_client
    Add a chat_client test case.

--- test/chat_client.cc
+++ test/chat_client.cc
@@ -1,10 +1,5 @@
-#include <unistd.h>
-
-#include <array>
-#include <cstring>
 #include <iostream>
 
-#include "Buffer.h"
 #include "Connection.h"
 #include "Socket.h"
 #include "util.h"
@@ -16,13 +11,6 @@ int main() {
     Connection conn(nullptr, socket);
 
     while (true) {
-        conn.getline_send_buffer();
-        conn.write();
-        if (conn.get_state() == Connection::State::Closed) {
-            conn.close();
-            break;
-        }
-
         conn.read();
         std::cout << "Message from server: " << conn.get_read_buffer() << std::endl;
     }
